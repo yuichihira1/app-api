3e821f1253b746ef13548ff0e2fb863d2851e5a5
hirayuichi <yhira5796@gmail.com>
2018-01-13 20:27:47 +0900

    scoresテーブルを作成
    references型でuser_idカラム（親モデルのカラム）を追加した。

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180113102617) do
+ActiveRecord::Schema.define(version: 20180113110906) do
+
+  create_table "scores", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
+    t.bigint "user_id"
+    t.integer "score"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_scores_on_user_id"
+  end
 
   create_table "users", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
     t.string "email", default: "", null: false
