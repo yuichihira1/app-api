bb7291fd6439075f0aafce8d99f743ff14f69229
hirayuichi <yhira5796@gmail.com>
2018-01-13 18:41:30 +0900

    [Authorization]ヘッダーを元にトークン認証を行う。

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,16 @@
 class ApplicationController < ActionController::API
+  include AbstractController::Translation
+
+  respond_to :json
+
+  def authenticate_user_from_token!
+    auth_token = request.headers['Authorization']
+
+    if auth_token
+      authenticate_with_uth_token auth_token
+    else
+      authenticate_error
+    end
+  end
+  
 end
