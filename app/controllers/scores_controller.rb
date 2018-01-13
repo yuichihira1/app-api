c2c5423ed8e81cf6b70a3e8e73a98f8e866b6cc7
hirayuichi <yhira5796@gmail.com>
2018-01-13 20:34:31 +0900

    showアクションでスコアの最大得点を１件取得する。

--- app/controllers/scores_controller.rb
+++ app/controllers/scores_controller.rb
@@ -2,6 +2,10 @@ class ScoresController < ApplicationController
   before_action :authenticate_user_from_token!, only: [:show, :create]
 
   def show
+    params[:email] = request.headers['EMAIL_NAME']
+    @user = User.find_by(email: params[:email])
+    @score =  @user.scores.maximum(:score)
+    render json: @score, each_serializer: ScoreSerializer
 
   end
 
@@ -19,5 +23,5 @@ class ScoresController < ApplicationController
   def score_params
     require(:score).params.permit(:user_id, :score)
   end
-  
+
   end
