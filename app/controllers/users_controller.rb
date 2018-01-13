445218ba88481e15b9e1c5f061a87c4651ad309d
hirayuichi <yhira5796@gmail.com>
2018-01-13 19:48:49 +0900

    ミスを修正

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -17,9 +17,9 @@ class UsersController < ApplicationController
   def create
     @user = User.new(user_params)
     if @user.save!
-      render json: @user, serializer: UserSerializer, root nil
+      render json: @user, serializer: UserSerializer, root: nil
     else
-      render json { error: t('user_create_error') }, status: :unprocessable_entity
+      render json: { error: t('user_create_error')}, status: :unprocessable_entity
     end
   end
 
@@ -29,10 +29,5 @@ class UsersController < ApplicationController
     params.require(:user).permit(:email, :password, :name)
   end
 
-
-
-
-
-
-
 end
+
