d7b430d7cea3d3de72eef4eca93c69495f2ed710
hirayuichi <yhira5796@gmail.com>
2018-01-13 19:11:02 +0900

    ルーティングを設定
    ・users#index（検証環境のトップページ）
    ・users#index（ユーザー一覧）
    ・users#create（ユーザー作成）
    ・users#show（ユーザー詳細）
    /
    ・sessions#create（ログイン）
    ・sessions#show(クライアントにセッションを返す)

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,14 @@
 Rails.application.routes.draw do
-  devise_for :users
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  devise_for :users, only: []
+  root to: 'users#index'
+
+  resource :login, only: [:create], controller: :sessions
+  resource :cookie, only: [:show], controller: :sessions
+  resource :show, only: [:show], controller: :users
+  resource :show, only: [:show], controller: :users
+  resources :users, only: [:index, :create, :show] do
+
+  end
+
+
 end
