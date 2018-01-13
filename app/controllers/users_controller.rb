04624980fd9209257ea33dbc576391b5ddd96d8a
hirayuichi <yhira5796@gmail.com>
2018-01-13 19:19:27 +0900

    トップページを作成
    Herokuなどにステージしたとき必要。GET通信すると簡単なメッセージを返す。

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,11 @@
 class UsersController < ApplicationController
   skip_before_action :authenticate_user_from_token!, only: [:index, :create]
-  
+
+　 def index
+  # サーバーにアクセスした時に表示する。
+    message = { top: 'Hello, API'}
+    render json: message
+end
 
 
 end
