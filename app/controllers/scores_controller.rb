d6e2223459989df633915e3a646476280dc7087a
hirayuichi <yhira5796@gmail.com>
2018-01-13 20:18:27 +0900

    Score保温機能を設定した。

--- app/controllers/scores_controller.rb
+++ app/controllers/scores_controller.rb
@@ -1,2 +1,23 @@
 class ScoresController < ApplicationController
-end
+  before_action :authenticate_user_from_token!, only: [:create, :show]
+
+  def show
+
+  end
+
+  def create
+    @score = Score.create(score_params)
+    if score.save
+      render json: @score, serializer: ScoreSerializer, root: nil
+    else
+      render json: { error: t('score_save_error') }, status: :unprocessable_entity
+    end
+  end
+
+  private
+
+  def score_params
+    require(:score).params.permit(:user_id, :score)
+  end
+  
+  end
