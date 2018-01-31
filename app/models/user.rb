c2c7de92026ec1fad1b65786e2d72463fadac0e1
hirayuichi <yhira5796@gmail.com>
2018-01-31 11:48:11 +0900

    Userモデルに子モデルScoreのレコードを追従させる。
    accepts_nested_attributes_forメソッドでScoreモデルを指定

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,6 +2,8 @@ class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
   has_many :scores
+  accepts_nested_attributes_for :scores
+
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
 
