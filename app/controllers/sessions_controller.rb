e305bb0800bae8fce54dff9dd0f574e0e2ef8bec
hirayuichi <yhira5796@gmail.com>
2018-01-31 11:50:56 +0900

    セッションをshowアクションでクライアントに反映しない。

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,19 +1,6 @@
 class SessionsController < ApplicationController
   skip_before_action :authenticate_user_from_token!
 
-  # Get session
-  def show
-    params[:email] = request.headers['HTTP_EMAIL']
-    params[:password] = request.headers['HTTP_PASSWORD']
-    @user = User.find_by(email: params[:email], password: params[:password])
-
-    if @user.nil?
-      render json: @user, serializer: SessionSerializer, root: nil
-    else
-      render json: { error: '404 error' }, status: 404
-    end
-  end
-
   # POST login
   def create
     @user = User.find_for_database_authentication(email: params[:email])
