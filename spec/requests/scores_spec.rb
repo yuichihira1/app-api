997e3e14ccb5a74c46b535fa19081b1607f0b9b8
hirayuichi <yhira5796@gmail.com>
2018-02-11 16:23:08 +0900

    spec/rais_herper.rbを生成、テストファイルに許可

--- spec/requests/scores_spec.rb
+++ spec/requests/scores_spec.rb
@@ -1,9 +1,19 @@
 require 'rails_helper'
+require 'spec_helper'
+
+
 
 RSpec.describe "Scores", type: :request do
-  describe "GET /scores" do
+  describe "GET /scoreget" do
     it "works! (now write some real specs)" do
-      get scores_path
+      get user_scoreget_path
+      expect(response).to have_http_status(200)
+    end
+  end
+
+  describe "POST /scorepost" do
+    it "works! (now write some real specs)" do
+      get user_scorepost_path
       expect(response).to have_http_status(200)
     end
   end
