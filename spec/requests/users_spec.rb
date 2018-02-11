be4a172f2b7208551398a06f5926bb7b5ad10e7d
hirayuichi <yhira5796@gmail.com>
2018-02-11 19:49:09 +0900

    Userのテストケースを設計

--- spec/requests/users_spec.rb
+++ spec/requests/users_spec.rb
@@ -1,11 +1,51 @@
 require 'rails_helper'
 require 'spec_helper'
 
+
 RSpec.describe "Users", type: :request do
+  include Rack::Test::Methods
+
+  #GET /users
   describe "GET /users" do
-    it "works! (now write some real specs)" do
-      get users_path
+    before(:each) do
+      @user = FactoryBot.create(:user)
+      get user_path(@user)
+      @json = JSON.parse(response.body)
+      application_json
+      login_user
+      authenticate
+    end
+
+    #トークン認証
+    it 'トークン認証する' do
+       GET '/user/1', {}, @env
+    end
+　　
+    #１件のユーザーを取得
+    it "200が返ってくる" do
+      get user_path
       expect(response).to have_http_status(200)
     end
   end
+
+  #POST/users
+  describe "POST /users" do
+
+    before(:each) do
+      application_json
+      @params = FactoryGirl.attributes_for(:user)
+    end
+
+    it '200が返ってくる' do
+      post users_path, @params
+      expect(response).to be_success
+      expect(response.status).to eq 200
+    end
+
+    it 'Userレコードが1増える' do
+      expect { post users_path, @params }.to change(User, :count).by(1)
+    end
+  end
+
+
 end
