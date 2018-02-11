d7c316ec62607d213e38be9f0e936401fe5c60b3
hirayuichi <yhira5796@gmail.com>
2018-02-11 19:43:09 +0900

    必要なスペックファイルのみ生成するようにした

--- config/application.rb
+++ config/application.rb
@@ -33,5 +33,17 @@ module AppApi
     config.to_prepare do
       DeviseController.respond_to :json
     end
+    config.generators do |g|
+      g.test_framework :rspec,
+                       fixtures: true,
+                       view_specs: false,
+                       helper_specs: true,
+                       routing_specs: false,
+                       controller_specs: false,
+                       request_specs: true
+      g.fixture_replacement :factory_bot, dir: "spec/factories"
+    end
   end
+
+
 end
